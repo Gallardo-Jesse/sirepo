21cf65365964768bf73c27ad9286f6cad61aab85
gurhar1133 <ghns1133@gmail.com>
2024-04-08 23:06:34 +0000

    Fix #6957: ckp

--- impact_example/impact_data_proc.py
+++ impact_example/impact_data_proc.py
@@ -1,10 +1,14 @@
 from impact import Impact
-# import lume.base
-# import rslume
-I = Impact('ImpactT.in', verbose=True)
-I.numprocs = 1
-I.stop = 0.16
-I.run()
-x = I.particles['final_particles'].x
-y = I.particles['final_particles'].y
-print("x =", x, "y =", y)
+
+
+def run_impact(fpath):
+    I = Impact(fpath, verbose=True)
+    I.numprocs = 1
+    I.stop = 0.16
+    I.run()
+    return I.particles['final_particles'].x, I.particles['final_particles'].y
+
+
+if __name__ == "__main__":
+    x, y = run_impact('ImpactT.in')
+    print("x =", x, "y =", y)
